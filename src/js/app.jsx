e2be57f90cf009644f0afe1b23cae6f94178c749
Max Kalhama <kalhama.max@gmail.com>
2021-11-27 14:00:08 +0200

    Add google analytics

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -5,6 +5,10 @@ import { Map } from './components/Map'
 import { Credits } from './components/Credits'
 import { Menu } from './components/Menu'
 import { NotFound } from './components/NotFound'
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-JPMWTY28WQ');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App = () => {
     return (
