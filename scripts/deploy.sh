d473aeb7904ebe3a2561c592c302d6a2ef8d72fa
Max Kalhama <kalhama.max@gmail.com>
2025-02-28 16:16:02 +0200

    feat: rebrand

--- scripts/deploy.sh
+++
@@ -1,4 +0,0 @@
-DIR=/var/www/metropaccess-visualiser-frontend
-yarn install
-yarn build
-rsync -avh --delete ./build/ max@metropaccess.max.kalhama.fi:$DIR/public_html/
--- src/js/components/Credits.jsx
+++ src/js/components/Credits.jsx
@@ -8,6 +8,6 @@ export const Credits = () => {
         <div>
             Visualisaation ohjelmistolähdekoodi, jonka tekijä on Max Kalhama on lisensoitu Creative Commons Nimeä 4.0 Kansainvälinen -lisenssillä
         </div>
-        <a href="https://github.com/metropaccess-visualiser">Sivuston lähdekoodi</a>. <a href="https://blogs.helsinki.fi/saavutettavuus/">Metropaccess-hankkeen nettisivut</a>
+        <a href="https://github.com/Kalhama/matka-aika">Sivuston lähdekoodi</a>. <a href="https://blogs.helsinki.fi/saavutettavuus/">Metropaccess-hankkeen nettisivut</a>
     </div>
 }
